d1aee41a3cc7478b8864c00172cf401879f28231
seandeo <adeoyeseun@hotmail.com>
2023-12-06 15:19:13 -0600

    new changes

---
+++ Models/ErrorViewModel.cs
@@ -0,0 +1,9 @@
+﻿namespace Dec6MVCWebApp.Models;
+
+public class ErrorViewModel
+{
+    public string? RequestId { get; set; }
+
+    public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+}
+
